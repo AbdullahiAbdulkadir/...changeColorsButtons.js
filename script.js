b1939b4b0708e4db1336fe9bcb2745ffb42983d4
Abdullahiabdulkadir <Abdullahiabdulkadir0213999@gmail.com>
2022-07-10 18:09:57 +0100

    change color button js

---
+++ script.js
@@ -0,0 +1,14 @@
+let button = document.querySelector("button"); 
+let box = document.getElementById("changeMe");
+
+function colorChange(){
+    if (box.style.background == "blue") {
+      box.style.background = "red";
+    } else {
+      box.style.background = "blue";
+    }
+    
+}
+
+
+// Topknot Clare JavaScript Basics in 10 Minutes (11:48)
